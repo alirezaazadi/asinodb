c6f8571d26039358b34fdd7ca1a236d1c9fee62e
Alireza Azadi <Alireza_Azadi@Hotmail.com>
2023-06-16 23:32:57 +0200

    http server - get method for get command - post method for set command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/alirezaazadi/asinodb.git"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -23,15 +24,7 @@ func getHandler(d *asinodb.Database) http.HandlerFunc {
 		value, err := d.Get(pathVars["key"])
 
 		if err != nil {
-			if err == asinodb.ErrNothing {
-				w.WriteHeader(http.StatusNotFound)
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
-			}
-
-			writeJSON(w, &struct {
-				Message string `json:"message"`
-			}{Message: err.Error()})
+			writeError(w, err)
 			return
 		}
 
@@ -43,10 +36,34 @@ func getHandler(d *asinodb.Database) http.HandlerFunc {
 
 func setHandler(d *asinodb.Database) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		pathVars := mux.Vars(r)
+		value, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+
+		if err := d.Set(pathVars["key"], string(value)); err != nil {
+			writeError(w, err)
+		}
 	}
 }
 
 func writeJSON(w http.ResponseWriter, data interface{}) {
 	encoder := json.NewEncoder(w)
 	encoder.Encode(data)
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	if err == asinodb.ErrNothing {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	writeJSON(w, &struct {
+		Message string `json:"message"`
+	}{Message: err.Error()})
 }
